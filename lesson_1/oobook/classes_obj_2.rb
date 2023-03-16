1edff23539ae806edda54309b69d3b736404fc81
davejlim <7742500+davejlim@users.noreply.github.com>
2023-03-16 14:45:30 -0700

    Continued Work on OO Book Classes and Objects II Execises

---
+++ lesson_1/oobook/classes_obj_2.rb
@@ -0,0 +1,48 @@
+=begin
+1. Add a class method to your MyCar class that calculates the gas mileage of any car.
+=end
+
+class MyCar
+    attr_accessor :year, :color, :model, :speed
+    
+    def initialize(year, color, model)
+        self.year = year
+        self.color = color
+        self.model = model
+        self.speed = 0
+    end
+
+    def self.calc_mileage
+        #calculate gas mileage
+    end
+
+    def to_s
+        
+    end
+end
+
+=begin
+2. Override the to_s method to create a user friendly print out of your object.
+
+3. When running the following code...
+
+class Person
+  attr_reader :name
+  def initialize(name)
+    @name = name
+  end
+end
+
+bob = Person.new("Steve")
+bob.name = "Bob"
+
+We get the following error...
+
+test.rb:9:in `<main>': undefined method `name=' for
+  #<Person:0x007fef41838a28 @name="Steve"> (NoMethodError)
+
+This is because we only created a getter method with our accessor method attr_reader. We would need to create its setter equivalent to be able to call that setter method
+and change the state of the object.
+=end
+
+p second_car = MyCar.new(1998, 'Black', 'Camry')
