d418c071f0be7fb959eb424538f8c4d27d3d719c
davejlim <7742500+davejlim@users.noreply.github.com>
2023-03-21 13:21:10 -0700

    OO Book Inheritance Exercises

--- lesson_1/oobook/inheritance.rb
+++ lesson_1/oobook/inheritance.rb
@@ -1,7 +1,7 @@
-class MyCar
-    attr_accessor :year, :color, :model, :speed
+class Vehicle
+   attr_accessor :year, :color, :model, :speed
     
-    def initialize(year, color, model)
+   def initialize(year, color, model)
         self.year = year
         self.color = color
         self.model = model
@@ -11,8 +11,23 @@ class MyCar
     def self.calc_mileage(miles, gallons)
       @mpg = miles/gallons
     end
+end
 
+class MyCar < Vehicle
+    
     def to_s
        "A #{self.year} #{self.model} car is going #{self.speed} miles per hour." 
     end
+
+    DOORS = 4
 end
+
+p second_car = MyCar.new(1998, 'Black', 'Camry')
+p MyCar.calc_mileage(100, 30)
+puts second_car
+
+class MyTruck < Vehicle
+
+    BED = true
+end
+
