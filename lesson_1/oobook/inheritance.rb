3eb9a2ad3f955fd9dae09f60854b2c61d938dfd6
davejlim <7742500+davejlim@users.noreply.github.com>
2023-03-22 16:10:31 -0700

    Inheritance Exercise

--- lesson_1/oobook/inheritance.rb
+++ lesson_1/oobook/inheritance.rb
@@ -1,33 +1,47 @@
 class Vehicle
-   attr_accessor :year, :color, :model, :speed
-    
-   def initialize(year, color, model)
-        self.year = year
-        self.color = color
-        self.model = model
-        self.speed = 0
-    end
+  attr_accessor :year, :color, :model, :speed
+  
+  @@vehicle_count = 0
+  
+  def initialize(year, color, model)
+      self.year = year
+      self.color = color
+      self.model = model
+      self.speed = 0
+      @@vehicle_count += 1
+  end
 
-    def self.calc_mileage(miles, gallons)
-      @mpg = miles/gallons
-    end
+  def self.calc_mileage(miles, gallons)
+    @mpg = miles/gallons
+  end
+
+  def self.vehicle_count
+    p "There are #{@@vehicle_count} vehicles."
+  end
 end
 
 class MyCar < Vehicle
-    
-    def to_s
-       "A #{self.year} #{self.model} car is going #{self.speed} miles per hour." 
-    end
-
-    DOORS = 4
+  def to_s
+    "A #{self.year} #{self.model} car is going #{self.speed} miles per hour." 
+  end
+  DOORS = 4
 end
-
-p second_car = MyCar.new(1998, 'Black', 'Camry')
-p MyCar.calc_mileage(100, 30)
-puts second_car
 
 class MyTruck < Vehicle
-
-    BED = true
+  def to_s
+    "A #{self.year} #{self.model} truck is going #{self.speed} miles per hour." 
+  end
+  BED = true
 end
 
+module Loadable
+  attr_accessor :load
+  def initialize
+  puts ""
+p second_car = MyCar.new(1998, 'Black', 'Camry')
+p MyCar.calc_mileage(100, 30)
+p first_truck = MyTruck.new(1969, 'Red', 'Chevy')
+puts second_car
+puts first_truck
+Vehicle.vehicle_count
+MyCar.vehicle_count
