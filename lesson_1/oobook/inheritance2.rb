be13a9b62d1a1f5d3e13113aa6688bb19c150e2c
davejlim <7742500+davejlim@users.noreply.github.com>
2023-03-25 11:41:54 -0700

    Inhertiance Excerise 7

--- lesson_1/oobook/inheritance2.rb
+++ lesson_1/oobook/inheritance2.rb
@@ -5,7 +5,6 @@ Create a better_grade_than? method, that you can call like so...
 
 class Student
   attr_accessor :name
-  attr_reader :grade
 
   GRADES = %w(A B C D F)
 
@@ -17,11 +16,14 @@ class Student
   def better_grade_than?(compared_student)
     GRADES.index(self.grade) < GRADES.index(compared_student.grade)
   end
+
+  protected
+  attr_reader :grade
 end
 
 p student1 = Student.new('Bob', 'a')
 p student2 = Student.new('Pete', 'b')
 p student1.better_grade_than?(student2)
-p student1.grade
-p student2.grade
+# p student1.grade
+# p student2.grade
 p student2.better_grade_than?(student1)
