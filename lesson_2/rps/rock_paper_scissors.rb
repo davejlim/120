417fc3af87880e6f45852034b5723a21d960b281
davejlim <7742500+davejlim@users.noreply.github.com>
2023-04-08 22:26:05 +0000

    Included Optional Features

--- lesson_2/rps/rock_paper_scissors.rb
+++ lesson_2/rps/rock_paper_scissors.rb
@@ -3,70 +3,53 @@
 class Move
   attr_reader :value
 
-  VALUES = ['rock', 'paper', 'scissors', 'spock', 'lizard']
-
-  def initialize(value)
-    @value = value
-  end
-
-  def scissors?
-    @value == 'scissors'
-  end
-
-  def rock?
-    @value == 'rock'
-  end
-
-  def paper?
-    @value == 'paper'
-  end
-
-  def spock?
-    @value == 'spock'
-  end
-
-  def lizard?
-    @value == 'lizard'
-  end
-
-  # Rock, Paper, Scissors, Spock, Lizard
-  # Rock beats
-  # - Scissors
-  # - Lizard
-  # Paper beats
-  # - Rock
-  # - Spock
-  # Scissors beats
-  # - Paper
-  # - Lizard
-  # Spock beats
-  # - Scissors
-  # - Rock
-  # Lizard beats
-  # - Paper
-  # - Spock
+  VALUES = ['Rock', 'Paper', 'Scissors', 'Spock', 'Lizard']
+  @@beatable_moves = { 'Rock': ['Scissors', 'Lizard'],
+                       'Paper': ['Rock', 'Spock'],
+                       'Scissors': ['Paper', 'Lizard'],
+                       'Spock': ['Scissors', 'Rock'],
+                       'Lizard': ['Paper', 'Spock'] }
 
   def >(compare_move)
-    if rock?
-      compare_move.scissors? || compare_move.lizard?
-    elsif scissors?
-      compare_move.paper? || compare_move.lizard?
-    elsif paper?
-      compare_move.rock? || compare_move.spock?
-    elsif spock?
-      compare_move.scissors? || compare_move.rock?
-    elsif lizard?
-      compare_move.paper? || compare_move.spock?
-    end
+    @@beatable_moves[self.class.name.to_sym].include?(compare_move.class.name)
+  end
+
+  def to_s
+    @value
   end
 end
 
+class Rock < Move
+  @beatable_moves = ['Scissors', 'Lizard']
+
+  def >(compare_move)
+    super
+  end
+end
+
+class Paper < Move
+  @beatable_moves = ['Paper', 'Lizard']
+end
+
+class Scissors < Move
+  @beatable_moves = ['Rock', 'Spock']
+end
+
+class Lizard < Move
+  @beatable_moves = ['Scissors', 'Rock']
+end
+
+class Spock < Move
+  @beatable_moves = ['Paper', 'Spock']
+end
+
 class Player
-  attr_accessor :move, :name, :score
+  attr_accessor :move, :name, :score, :move_history
 
   def initialize
     set_name
     self.score = 0
+    @move_history = []
   end
 end
 
@@ -83,22 +66,69 @@ class Human < Player
   def choose
     choice = nil
     loop do
-      puts "Please choose rock, paper, or scissors:"
-      choice = gets.chomp.downcase
+      puts "Please choose Rock, Paper, Scissors, Lizard, or Spock:"
+      choice = gets.chomp.downcase.capitalize
       break if Move::VALUES.include? choice
-      puts "Sorry, invalid choice."
+      puts "Sorry, invalid choice. Please write one of the listed choices."
     end
-    self.move = Move.new(choice)
+    self.move = Object.const_get(choice).new
+    @move_history << move.class.name
   end
 end
 
 class Computer < Player
-  def set_name
-    self.name = ['R2D2', 'Hal', 'Chappie', 'Number 5'].sample
+  @@computer_names = ['R2D2', 'Hal', 'Chappie', 'Number']
+
+  def self.computer_names
+    @@computer_names
   end
 
   def choose
-    self.move = Move.new(Move::VALUES.sample)
+    @move_history << move.class.name
+  end
+end
+
+class R2D2 < Computer
+  def set_name
+    self.name = 'R2D2'
+  end
+
+  def choose
+    self.move = Rock.new
+    super
+  end
+end
+
+class Hal < Computer
+  def set_name
+    self.name = 'Hal'
+  end
+
+  def choose
+    self.move = Object.const_get(Move::VALUES[0, 2].sample).new
+    super
+  end
+end
+
+class Chappie < Computer
+  def set_name
+    self.name = 'Chappie'
+  end
+
+  def choose
+    self.move = Object.const_get(Move::VALUES[1, 3].sample).new
+    super
+  end
+end
+
+class Number < Computer
+  def set_name
+    self.name = 'Number 5'
+  end
+
+  def choose
+    self.move = Spock.new
+    super
   end
 end
 
@@ -107,25 +137,35 @@ class RPSGame
 
   def initialize
     @human = Human.new
-    @computer = Computer.new
+    @computer = Object.const_get(Computer.computer_names.sample).new
+  end
+
+  def display_border
+    puts "+------------------------------------------------------------+"
   end
 
   def display_welcome_message
-    puts "Welcome #{human.name} to Rock, Paper, Scissors!"
+    display_border
+    puts "Welcome #{human.name} to Rock, Paper, Scissors, Lizard, Spock!"
     puts "The first person to 10 points wins the game. Good luck!"
+    display_border
   end
 
   def display_goodbye_message
-    puts "Thanks for playing Rock, Paper, Scissors. Goodbye!"
+    display_border
+    puts "Thanks for playing Rock, Paper, Scissors, Lizard, Spock. Goodbye!"
   end
 
   def display_moves
-    puts "#{human.name} chose #{human.move.value}."
-    puts "#{computer.name} chose #{computer.move.value}."
+    display_border
+    puts "#{human.name} chose #{human.move.class.name}."
+    puts "#{computer.name} chose #{computer.move.class.name}."
+    display_border
   end
 
   def display_score
     puts "Scores: Human - #{human.score} | Computer #{computer.score}"
+    display_border
   end
 
   def display_round_winner
@@ -137,6 +177,27 @@ class RPSGame
       puts "It's a tie!"
     end
     display_score
+  end
+
+  def prompt_move_history
+    answer = nil
+
+    loop do
+      puts "Would you like to see the current move history? (y/n)"
+      answer = gets.chomp.downcase
+      break if ['y', 'n'].include? answer.downcase
+      puts "Sorry, your response must be y or n."
+    end
+
+    display_move_history if answer == 'y'
+    display_border
+  end
+
+  def display_move_history
+    display_border
+    puts "Move History:"
+    puts "#{human.name}: #{human.move_history}"
+    puts "#{computer.name}: #{computer.move_history}"
   end
 
   def human_won
@@ -155,7 +216,7 @@ class RPSGame
       puts "Would you like to play again? (y/n)"
       answer = gets.chomp.downcase
       break if ['y', 'n'].include? answer.downcase
-      puts "Sorry, must be y or n."
+      puts "Sorry, your response must be y or n."
     end
 
     answer == 'y'
@@ -181,6 +242,7 @@ class RPSGame
       computer.choose
       display_moves
       display_round_winner
+      prompt_move_history
       break if end_game?
     end
   end
