6ea1a5458ac8385abe0dcdfcc39a84d58f0e373f
davejlim <7742500+davejlim@users.noreply.github.com>
2023-06-19 23:48:54 +0000

    3 of Improvements

--- tic_tac_toe/ttt.rb
+++ tic_tac_toe/ttt.rb
@@ -105,8 +105,7 @@ class TTTGame
     puts "Thanks for playing Tic Tac Toe! Goodbye"
   end
 
-  def display_board(clear_screen: true)
-    clear if clear_screen
+  def display_board
     puts "You're a #{human.marker}. Computer is a #{computer.marker}."
     puts ""
     puts "     |     |"
@@ -121,6 +120,11 @@ class TTTGame
     puts "  #{board.get_square_at(7)}  |  #{board.get_square_at(8)}  |  #{board.get_square_at(9)}"
     puts "     |     |"
     puts ""
+  end
+
+  def clear_screen_and_display_board
+    clear
+    display_board
   end
 
   def human_moves
@@ -174,7 +178,7 @@ class TTTGame
     display_welcome_message
 
     loop do
-      display_board(clear_screen: false)
+      display_board
 
       loop do
         human_moves
@@ -183,7 +187,7 @@ class TTTGame
         computer_moves
         break if board.someone_won? || board.full?
 
-        display_board
+        clear_screen_and_display_board
       end
 
       display_result
